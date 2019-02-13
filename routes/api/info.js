bfae497250ec3a9fc6d806bc2ebc94b0dcae2784
morafer <fercho2296@gmail.com>
2019-02-12 19:29:52 -0600

    Agregando Nuevas Empresas

--- routes/api/info.js
+++ routes/api/info.js
@@ -29,4 +29,20 @@ router.get('/', function( req, res, next) {
     }
   });
 
+  // Agregar Una Nueva Empresa
+router.post('/new', function(req, res, next){
+    var info = Object.assign({} , informacion, req.body);
+    if(!data){
+      data = [];
+    }
+    data.push(info);
+    fileModel.write(data, function(err){
+      if(err){
+        console.log(err);
+        return res.status(500).json({ 'error': 'Error al Obtener Informacion' });
+      }
+      return res.status(200).json(info);
+    });
+  });
+
 module.exports = router;
