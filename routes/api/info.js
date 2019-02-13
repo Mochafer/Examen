52240995248e0f1b892bf4d9a203d1d6d4ab0851
morafer <fercho2296@gmail.com>
2019-02-12 19:29:12 -0600

    Mostrando La Informacion

--- routes/api/info.js
+++ routes/api/info.js
@@ -12,6 +12,21 @@ var informacion = {
   'Telefono': null 
 };
 
-
+//Obtener TODAS LAS EMPRESAS
+router.get('/', function( req, res, next) {
+    if(!data){
+      fileModel.read(function(err, filedata){
+        if(err){
+          console.log(err);
+          data = [];
+          return res.status(500).json({'error':'Error al Obtener Data'});
+        }
+        data = JSON.parse(filedata);
+        return res.status(200).json(data);
+      });
+    } else {
+      return res.status(200).json(data);
+    }
+  });
 
 module.exports = router;
